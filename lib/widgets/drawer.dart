2129949d1bece0af76d8651a0bee17c5528cfcbf
Michael Baptiswa Marully <baptiswamichael@gmail.com>
2022-12-13 01:21:13 +0700

    fix drawer

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: use_build_context_synchronously
 
-import 'package:evryday/screencs/evorums/forum.dart';
+import 'package:evryday/screencs/evorums/evorums.dart';
 import 'package:evryday/screens/evishlist/evishlist_home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:evryday/main.dart';
@@ -139,7 +139,7 @@ class DrawerComponents extends StatelessWidget {
               Provider.of<ScreenState>(context, listen: false)
                   .setCurrentScreen(2);
               Navigator.pushReplacement(context,
-                  MaterialPageRoute(builder: (context) => const HomePage()));
+                  MaterialPageRoute(builder: (context) => const InForum()));
             }
           },
         ),

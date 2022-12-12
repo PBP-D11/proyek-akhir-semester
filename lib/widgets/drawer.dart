263f30365f75b5881065ea8965020aab91dd59e1
nadhifralfan <nadhif.rahman11@ui.ac.id>
2022-12-12 18:55:20 +0700

    Update profile

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -9,7 +9,7 @@ import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:evryday/screens/signup.dart';
 import 'package:evryday/screens/profile.dart';
 // import '../../evishlist/lib/screen/evishlist_home_page.dart';
-import 'package:evices/screens/evices.dart';
+// import 'package:evices/screens/evices.dart';
 import 'package:provider/provider.dart';
 
 class DrawerComponents extends StatelessWidget {
@@ -97,7 +97,7 @@ class DrawerComponents extends StatelessWidget {
                     context,
                     MaterialPageRoute(
                         //TODO: Ubah ke ServicesPage()
-                        builder: (context) => const ServicesPage()));
+                        builder: (context) => const HomePage()));
               }
             },
           ),
